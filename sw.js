a38efbb4b237b8c5b46038a5612b97b3234762c9
DustinKang60 <jonghoon.kang@gmail.com>
2026-06-17 14:17:35 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,28 @@
-// KLBnuri Service Worker v2
-const CACHE_NAME = 'klbnuri-202606170447';
+const CACHE_NAME = 'klbnuri-v1';
+const ASSETS = [
+  '/klbnuri/',
+  '/klbnuri/index.html',
+  '/klbnuri/manifest.json',
+];
 
-self.addEventListener('install', function(e) {
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', function(e) {
+self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(function(keys) {
-      return Promise.all(
-        keys.filter(function(k) { return k !== CACHE_NAME; })
-            .map(function(k) { return caches.delete(k); })
-      );
-    }).then(function() {
-      return self.clients.claim();
-    })
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
   );
+  self.clients.claim();
 });
 
-self.addEventListener('fetch', function(e) {
+self.addEventListener('fetch', e => {
   e.respondWith(
-    fetch(e.request).catch(function() {
-      return caches.match(e.request);
-    })
+    caches.match(e.request).then(cached => cached || fetch(e.request))
   );
 });
